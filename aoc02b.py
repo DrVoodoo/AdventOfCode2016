c2ae457cd0adf58de996a8bdb46cf8bea2907d9c
Kristian Rödin <kristian_rodin@hotmail.com>
2016-12-10 00:32:32 +0100

    Added second day part b

--- aoc02b.py
+++ aoc02b.py
@@ -16,21 +16,25 @@ class Instructions(object):
 class Keypad(object):
     """Keypad to simulate every row"""
     def __init__(self):
-        self.pad = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        self.pad = [
+            [0, 0, 1, 0, 0],
+            [0, 2, 3, 4, 0],
+            [5, 6, 7, 8, 9],
+            [0, 'A', 'B', 'C', 0],
+            [0, 0, 'D', 0, 0]]
         self.direction = ''
         self.x_pos = 1
         self.y_pos = 1
     def move(self, direction):
         """Move one step on the pad"""
-        if direction == 'U' and self.y_pos > 0:
+        if direction == 'U' and self.y_pos > 0 and self.pad[self.y_pos-1][self.x_pos] != 0:
             self.y_pos = self.y_pos - 1
-        if direction == 'D' and self.y_pos < 2:
+        if direction == 'D' and self.y_pos < 4 and self.pad[self.y_pos+1][self.x_pos] != 0:
             self.y_pos = self.y_pos + 1
-        if direction == 'L' and self.x_pos > 0:
+        if direction == 'L' and self.x_pos > 0 and self.pad[self.y_pos][self.x_pos-1] != 0:
             self.x_pos = self.x_pos - 1
-        if direction == 'R' and self.x_pos < 2:
+        if direction == 'R' and self.x_pos < 4 and self.pad[self.y_pos][self.x_pos+1] != 0:
             self.x_pos = self.x_pos + 1
-        #print direction, self.x_pos, self.y_pos
         self.direction = direction
     def code(self):
         """Get current keypad code"""
